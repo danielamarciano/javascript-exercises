fa98c3668fd9b67e4f23be55b3d562285b4c1b11
danielamarciano <danielalmarciano@gmail.com>
2022-11-18 20:06:35 -0500

    Create function for test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let string =[]
+    for (let i=0; i < 3; i++){
+    console.log(string.join(''))
+
+    }
 
 };
 
